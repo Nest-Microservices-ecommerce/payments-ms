9e1f9a183b13c3c44d11ceb247f9c6024daedcd5
bryan lopez <blopez4434@gmail.com>
2024-06-03 13:27:40 -0400

    Add class-transformer and class-validator dependencies

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  public async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `http://localhost:${envs.port}/payments/success`,
+      cancel_url: `http://localhost:${envs.port}/payments/cancelled`,
+    });
+    return session;
+  }
 }
