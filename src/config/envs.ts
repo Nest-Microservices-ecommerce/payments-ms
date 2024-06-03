2e042499e5c3039b36dd1c9edbd603f51a9ca008
bryan lopez <blopez4434@gmail.com>
2024-06-03 14:56:09 -0400

    Add environment variables for endpoint secrets, success URL, and cancel URL

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,18 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET: string;
+  ENDPOINT_SECRET: string;
+  SUCCESS_URL: string;
+  CANCEL_URL: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     STRIPE_SECRET: joi.string().required(),
+    ENDPOINT_SECRET: joi.string().required(),
+    SUCCESS_URL: joi.string().required(),
+    CANCEL_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -24,4 +30,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecret: envVars.STRIPE_SECRET,
+  endpointSecret: envVars.ENDPOINT_SECRET,
+  successUrl: envVars.SUCCESS_URL,
+  cancelUrl: envVars.CANCEL_URL,
 };
